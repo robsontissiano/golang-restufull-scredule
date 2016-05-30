0c3fd5d8ca107dd7450bb20c4ca592f8ba4969bc
Reweb <rgoncalves@reweb.com.br>
2016-05-30 15:29:20 -0300

    added log function

--- routes.go
+++ routes.go
@@ -14,7 +14,7 @@ type Route struct {
 }
 
 type Routes []Route
-
+/*
 func NewRouter() *mux.Router {
 
     router := mux.NewRouter().StrictSlash(true)
@@ -24,6 +24,25 @@ func NewRouter() *mux.Router {
             Path(route.Pattern).
             Name(route.Name).
             Handler(route.HandlerFunc)
+    }
+
+    return router
+}
+*/
+func NewRouter() *mux.Router {
+
+    router := mux.NewRouter().StrictSlash(true)
+    for _, route := range routes {
+        var handler http.Handler
+
+        handler = route.HandlerFunc
+        handler = Logger(handler, route.Name)
+
+        router.
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Handler(handler)
     }
 
     return router
